243dfddcd067bdf6ee59384b291e2787467dd35f
aneriadani <aneriadani12345@gmail.com>
2024-08-25 23:57:44 +0530

    two sum leetcode problem

---
+++ twoSum.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class twoSum {
+        public int[] twoSum (int[] nums, int target) {
+
+            Map<Integer, Integer> map = new HashMap<>();
+
+            for (int i = 0; i<= nums[i]-1; i++) {
+
+                int num2 = target - nums[i];
+
+                if(map.containsKey(num2)) {
+                    return new int[] {map.get(num2), i};
+                }
+                map.put(nums[i], i);
+            }
+            throw new IllegalArgumentException("Can't find sum of the numbers");
+        }
+    }
